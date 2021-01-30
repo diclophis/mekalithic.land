d60aa966a5c087cc0106ae4a92ac231638d07280
Jon Bardin <diclophis@gmail.com>
2021-01-30 05:45:43 -0800

    more wip

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM wkndr:latest
 
-COPY resources/* /var/lib/wkndr/resources/
+#COPY resources/* /var/lib/wkndr/resources/
+COPY public/* /var/lib/wkndr/public/
 COPY Wkndrfile /var/lib/wkndr/
 
-RUN ls -lh /var/lib/wkndr/resources
+#RUN ls -lh /var/lib/wkndr/resources
+RUN ls -lh /var/lib/wkndr/public
 
 RUN /var/lib/wkndr/iterate-web.sh
--- public/mekalithic.js
+++ public/mekalithic.js
@@ -4,15 +4,34 @@ console.log(THREE);
 
 var camera, controls, scene, renderer;
 
+
+function clientWidth() {
+  //return document.documentElement.clientWidth;
+  return document.body.clientWidth;
+}
+
+function clientHeight() {
+  //return document.documentElement.clientHeight;
+  return document.body.clientHeight;
+}
+
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.aspect = clientWidth() / clientHeight();
 	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( clientWidth(), clientHeight() );
 	render();
 }
 
 function init() {
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 100, 2000000 );
+
+//let element = document.getElementById("top");
+//while (element.firstChild) {
+//  element.removeChild(element.firstChild);
+//}
+
+  document.body.style = "padding: 0; margin: 0; width: 100%; height: 100%; position: absolute; overflow: hidden; background: black;";
+
+	camera = new THREE.PerspectiveCamera( 60, clientWidth() / clientHeight(), 100, 2000000 );
 
 	scene = new THREE.Scene();
 
@@ -21,8 +40,11 @@ function init() {
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+	renderer.setSize( clientWidth(), clientHeight() );
+
+  var sceneContainer = document.getElementById("scene-container");
+
+	sceneContainer.appendChild( renderer.domElement );
 
 	//controls = new OrbitControls( camera, renderer.domElement );
 	//controls.addEventListener( 'change', render );
